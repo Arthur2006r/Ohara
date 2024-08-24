6a50a0a342f7be5e5ed052ff11bf7f86415b486c
Arthur Andrade Reis <arthurreis06pessoal@gmail.com>
2024-08-24 16:07:56 -0300

    Adição do projeto da api, do arquivo do banco de dados e da pasta do código ionic
    Foi colocado apenas as pastas de onde vai ficar cada conteúdo.

---
+++ ohara/src/main/java/br/com/ohara/OharaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ohara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OharaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OharaApplication.class, args);
+	}
+
+}
---
+++ ohara/src/test/java/br/com/ohara/OharaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ohara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OharaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
