01e52957c175e31e90e954ea5dc53d68a719a7f7
Daniel Loures <daniellourescupertino11@gmail.com>
2024-08-25 22:09:20 -0300

    Modificações no código HTML
    Modificações nos códigos HTML e CSS da página inicial e de login.

--- ionic/ohara/src/app/app.component.ts
+++ ionic/ohara/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { addIcons } from 'ionicons';
+import { eye, lockClosed } from 'ionicons/icons';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,8 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+
+  addIcons({ eye, lockClosed});
+  }
 }
